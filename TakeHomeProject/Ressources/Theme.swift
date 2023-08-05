6c3fdaf259a8af29a173a44b2877b5ad8428d9ff
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-08-05 12:29:07 +0200

    - Add assets Image and logo - Create launch screen

---
+++ TakeHomeProject/Ressources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  TakeHomeProject
+//
+//  Created by Eric on 05/08/2023.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
