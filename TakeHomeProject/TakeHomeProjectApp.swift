5fb0dd996baf4f40c069256b82da6837e6e902e9
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-08-05 11:59:47 +0200

    Initial Commit

---
+++ TakeHomeProject/TakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeProjectApp.swift
+//  TakeHomeProject
+//
+//  Created by Eric on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
