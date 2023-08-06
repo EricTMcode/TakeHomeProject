2a275e0c7b83355ec2e6eebf13f0468008a809c1
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-08-06 12:49:09 +0200

    - Create PeopleView

--- TakeHomeProject/Features/People/Views/PeopleView.swift
+++ TakeHomeProject/Features/People/Views/PeopleView.swift
@@ -8,9 +8,59 @@
 import SwiftUI
 
 struct PeopleView: View {
+    private let columns = Array(repeating: GridItem(.flexible()), count: 2)
+    
     var body: some View {
         NavigationStack {
-            Text("People View")
+            ZStack {
+                Theme.background
+                    .ignoresSafeArea(edges: .top)
+                
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 16) {
+                        ForEach(0...5, id: \.self) { item in
+                            VStack(spacing: 0) {
+                                Rectangle()
+                                    .fill(.blue)
+                                    .frame(height: 130)
+                                
+                                VStack(alignment: .leading) {
+                                    Text("#\(item)")
+                                        .font(.system(.caption, design: .rounded))
+                                        .bold()
+                                        .foregroundColor(.white)
+                                        .padding(.horizontal, 9)
+                                        .padding(.vertical, 4)
+                                        .background(Theme.pill, in: Capsule())
+                                    
+                                    Text("<First name> <Last name>")
+                                        .foregroundColor(Theme.text)
+                                        .font(.system(.body, design: .rounded))
+                                }
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.horizontal, 8)
+                                .padding(.vertical, 5)
+                                .background(Theme.detailBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                            .shadow(color: Theme.text.opacity(0.1), radius: 2, x: 0, y: 1)
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("People")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        
+                    } label: {
+                        Symbols.plus
+                            .font(.system(.headline, design: .rounded))
+                            .bold()
+                    }
+                }
+            }
         }
     }
 }
