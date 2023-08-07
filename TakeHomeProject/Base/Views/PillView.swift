0e3b88f97c67de60e3c72fdbbdeebaf12b37ba5a
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-08-07 13:20:26 +0200

    - Refactor PeopleView - Extracting view and create private extension

---
+++ TakeHomeProject/Base/Views/PillView.swift
@@ -0,0 +1,30 @@
+//
+//  PillView.swift
+//  TakeHomeProject
+//
+//  Created by Eric on 07/08/2023.
+//
+
+import SwiftUI
+
+struct PillView: View {
+    let id: Int
+    
+    var body: some View {
+        Text("#\(id)")
+            .font(.system(.caption, design: .rounded))
+            .bold()
+            .foregroundColor(.white)
+            .padding(.horizontal, 9)
+            .padding(.vertical, 4)
+            .background(Theme.pill, in: Capsule())
+    }
+}
+
+struct PillView_Previews: PreviewProvider {
+    static var previews: some View {
+        PillView(id: 0)
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
