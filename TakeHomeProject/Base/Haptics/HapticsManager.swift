2d4076486cb529653cecababb8665981ee43f84d
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-08-17 13:57:43 +0200

    - Create HapticsManger, and add haptic feedback to CreateView

---
+++ TakeHomeProject/Base/Haptics/HapticsManager.swift
@@ -0,0 +1,25 @@
+//
+//  HapticsManager.swift
+//  TakeHomeProject
+//
+//  Created by Eric on 17/08/2023.
+//
+
+import Foundation
+import UIKit
+
+fileprivate final class HapticsManager {
+    static let shared = HapticsManager()
+    
+    private let feedback = UINotificationFeedbackGenerator()
+    
+    private init() {}
+    
+    func trigger(_ notification: UINotificationFeedbackGenerator.FeedbackType) {
+        feedback.notificationOccurred(notification)
+    }
+}
+
+func haptic(_ notification: UINotificationFeedbackGenerator.FeedbackType) {
+    HapticsManager.shared.trigger(notification)
+}
