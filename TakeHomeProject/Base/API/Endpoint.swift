68b5f7c9657001c30b0dcc2f63deb971107566f8
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-08-18 14:31:17 +0200

    - Add enumType in endpoint

--- TakeHomeProject/Base/API/Endpoint.swift
+++ TakeHomeProject/Base/API/Endpoint.swift
@@ -10,11 +10,49 @@ import Foundation
 enum Endpoint {
     case people
     case detail(id: Int)
-    case create
+    case create(submissionData: Data?)
 }
 
 extension Endpoint {
-    var host: String { "" }
+    enum MethodType {
+        case GET
+        case POST(data: Data?)
+    }
+}
+
+extension Endpoint {
+    var host: String { "reqres.in" }
     
-    var path: String { "" }
+    var path: String {
+        switch self {
+        case .people, .create:
+            return "/api/users"
+        case .detail(let id):
+            return "/api/users/\(id)"
+        }
+    }
+    
+    var methodType: MethodType {
+        switch self {
+        case .people, .detail:
+            return .GET
+        case .create(let data):
+            return .POST(data: data)
+        }
+    }
+}
+
+extension Endpoint {
+    var url: URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = host
+        urlComponents.path = path
+        
+        #if DEBUG
+        urlComponents.queryItems = [ URLQueryItem(name: "delay", value: "1")]
+        #endif
+        
+        return urlComponents.url
+    }
 }
