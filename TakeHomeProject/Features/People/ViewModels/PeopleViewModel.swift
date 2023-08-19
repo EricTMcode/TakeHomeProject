50e68107a7044651d77e1395c2df6b3ccd5e8e0d
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-08-19 14:43:05 +0200

    - Implement Infinite scrolling & pagination

--- TakeHomeProject/Features/People/ViewModels/PeopleViewModel.swift
+++ TakeHomeProject/Features/People/ViewModels/PeopleViewModel.swift
@@ -10,16 +10,30 @@ import Foundation
 final class PeopleViewModel: ObservableObject {
     @Published private(set) var users: [User] = []
     @Published private(set) var error: NetworkingManager.NetworkingError?
-    @Published private(set) var isLoading = false
+    @Published private(set) var viewState: ViewState?
     @Published var hasError = false
     
+    private var page = 1
+    private var totalPages: Int?
+    
+    
+    var isLoading: Bool {
+        viewState == .loading
+    }
+    
+    var isFetching: Bool {
+        viewState == .fetching
+    }
+        
     @MainActor
     func fetchUsers() async {
-        isLoading = true
-        defer { isLoading = false }
+        reset()
+        viewState = .loading
+        defer { viewState = .finished }
         
         do {
-            let response = try await NetworkingManager.shared.request(.people, type: UsersResponse.self)
+            let response = try await NetworkingManager.shared.request(.people(page: page), type: UsersResponse.self)
+            self.totalPages = response.totalPages
             self.users = response.data
         } catch {
             self.hasError = true
@@ -28,6 +42,52 @@ final class PeopleViewModel: ObservableObject {
             } else {
                 self.error = .custom(error: error)
             }
+        }
+    }
+    
+    @MainActor
+    func fetchNextSetOfUsers() async {
+        guard page != totalPages else { return }
+        
+        viewState = .fetching
+        defer { viewState = .finished }
+        
+        page += 1
+        
+        do {
+            let response = try await NetworkingManager.shared.request(.people(page: page), type: UsersResponse.self)
+            self.totalPages = response.totalPages
+            self.users += response.data
+        } catch {
+            self.hasError = true
+            if let networkingError = error as? NetworkingManager.NetworkingError {
+                self.error = networkingError
+            } else {
+                self.error = .custom(error: error)
+            }
+        }
+    }
+    
+    func hasReachEnd(of user: User) -> Bool {
+        users.last?.id == user.id
+    }
+}
+
+extension PeopleViewModel {
+    enum ViewState {
+        case fetching
+        case loading
+        case finished
+    }
+}
+
+private extension PeopleViewModel {
+    func reset() {
+        if viewState == .finished {
+            users.removeAll()
+            page = 1
+            totalPages = nil
+            viewState = nil
         }
     }
 }
